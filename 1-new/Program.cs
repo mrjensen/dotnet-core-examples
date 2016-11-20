e5f2e6f6e579eef625764c2b57f8b77f4b2ac370
Martin Jensen <martin2610@gmail.com>
2016-11-20 23:57:48 +0100

    Added input/output terminal demo

--- 1-new/Program.cs
+++ 1-new/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello. Enter your name.");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
